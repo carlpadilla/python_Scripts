2b8f6596ae886ff72239b57b5fcfefea3cb58f2c
Carl Padilla <carlpadilla@protonmail.com>
2020-08-18 13:06:46 -0400

    saving changes

--- images.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageFilter
-
-img = Image.open('./Pokedex/charmander.jpg')
-filtered_img = img.filter(ImageFilter.BLUR)
-
-
-filtered_img.save('Blurred.png', 'png')
-filtered_img.show()
-print(img)
